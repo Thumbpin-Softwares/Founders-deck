cb97d3178a657ae5c02ea476fc0a7d60f30e4220
Sohan Rout <sohanrout84@gmail.com>
2026-07-03 13:03:58 +0530

    Feat : initial setup

---
+++ modules/common/components/smooth-sroll/index.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { ReactNode, useEffect } from "react";
+import Lenis from "lenis";
+
+export default function SmoothScroll({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+      duration: 1.2,
+      smoothWheel: true,
+      touchMultiplier: 2,
+    });
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
---
+++ modules/home/components/notification-strip/index.tsx
@@ -0,0 +1,5 @@
+export default function NotificationStrip(){
+    return(
+        <main></main>
+    );
+}
